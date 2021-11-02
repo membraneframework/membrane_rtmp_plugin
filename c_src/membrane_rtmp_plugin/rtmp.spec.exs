684f6328766d10f407e67430e67fb0f58cf9cb48
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2021-11-02 10:02:13 +0100

    Marking native_create as dirty

--- c_src/membrane_rtmp_plugin/rtmp.spec.exs
+++ c_src/membrane_rtmp_plugin/rtmp.spec.exs
@@ -8,3 +8,5 @@ spec native_create(string, timeout :: string) :: {:ok :: label, state} | {:error
 spec get_video_params(state) :: {:ok :: label, params :: payload} | {:error :: label, :no_stream}
 spec get_audio_params(state) :: {:ok :: label, params :: payload} | {:error :: label, :no_stream}
 spec read_frame(state) :: {:ok, :audio :: label, frame :: payload} | {:ok, :video :: label, frame :: payload} | {:error :: label, reason :: string} | (:end_of_stream :: label)
+
+dirty :io, native_create: 1
