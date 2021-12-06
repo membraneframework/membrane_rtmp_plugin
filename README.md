51e0c042cb47b8bc28d5a6b69d7ef4a81a91ace2
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2021-12-06 13:03:40 +0100

    Bugfixes: Source refusing to terminate and honoring timestamps (#7)

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ The package can be installed by adding `membrane_rtmp_plugin` to your list of de
 ```elixir
 def deps do
   [
-    {:membrane_rtmp_plugin, "~> 0.2.0"}
+    {:membrane_rtmp_plugin, "~> 0.2.1"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTMP.Mixfile do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @github_url "https://github.com/membraneframework/membrane_rtmp_plugin"
 
   def project do
@@ -42,11 +42,10 @@ defmodule Membrane.RTMP.Mixfile do
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
       {:credo, "~> 1.5", runtime: false},
-      {:membrane_file_plugin, "~> 0.6"},
+      {:membrane_file_plugin, "~> 0.6", only: :test},
       {:membrane_aac_format, "~> 0.3"},
-      {:membrane_element_fake, "~> 0.5"},
       {:membrane_h264_ffmpeg_plugin, "~> 0.12"},
-      {:membrane_aac_plugin, "~> 0.8"},
+      {:membrane_aac_plugin, "~> 0.9"},
       {:ffmpex, "~> 0.7", only: :test}
     ]
   end
