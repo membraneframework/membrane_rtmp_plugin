f0771ecb9a95f0e3dc803920201392629fbf8b9b
varsill <lukasz.kita0@gmail.com>
2022-05-16 13:08:24 +0200

    Improve formatting

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Membrane.RTMP.Mixfile do
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
       {:credo, "~> 1.5", runtime: false},
       {:membrane_hackney_plugin, "~> 0.8.0", only: :test},
-      {:membrane_h264_ffmpeg_plugin, github: "membraneframework/membrane_h264_ffmpeg_plugin", branch: "parser_integration_test"},
+      {:membrane_h264_ffmpeg_plugin,
+       github: "membraneframework/membrane_h264_ffmpeg_plugin", branch: "parser_integration_test"},
       {:membrane_aac_plugin, "~> 0.12.1"},
       {:ffmpex, "~> 0.7", only: :test},
       {:membrane_mp4_plugin, "~> 0.14.0"}
