835e5e52748b382dbb94f79f4c60d173f2994585
Mateusz Front <mateusz.front@swmansion.com>
2022-05-19 14:44:45 +0200

    bump to 0.4.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTMP.Mixfile do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.4.0"
   @github_url "https://github.com/membraneframework/membrane_rtmp_plugin"
 
   def project do
@@ -38,7 +38,7 @@ defmodule Membrane.RTMP.Mixfile do
   defp deps do
     [
       {:membrane_core, "~> 0.8.0"},
-      {:unifex, "~> 0.7.0"},
+      {:unifex, "~> 1.0"},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
       {:credo, "~> 1.5", runtime: false},
