8687e8a023cee814404f084feb1911dbd641a3cf
Przemyslaw Roznawski <roznawski.przemyslaw@gmail.com>
2022-09-08 15:56:22 +0200

    Fixed moduledoc.

--- lib/membrane_rtmp_plugin/rtmp/source/tcp_server.ex
+++ lib/membrane_rtmp_plugin/rtmp/source/tcp_server.ex
@@ -2,42 +2,9 @@ defmodule Membrane.RTMP.Source.TcpServer do
   @moduledoc """
   A simple tcp server, which executes given function for each new incoming connection.
 
-  ## Example
-  ```
-  alias Membrane.RTMP.Source.TcpServer
-
-  ip_addr = {127, 0, 0, 1}
-  port = 9000
-
-  server_options = %TcpServer{
-  port: port,
-  listen_options: [
-    :binary,
-    packet: :raw,
-    active: false,
-    ip: ip_addr
-  ],
-  socket_handler: fn socket ->
-    {:ok, receive_task} =
-      Task.start(fn ->
-        {:ok, data} = :gen_tcp.recv(socket, 0)
-        IO.inspect(data, label: "Received")
-      end)
-
-    {:ok, receive_task}
-  end
-  }
-
-  TcpServer.start_link(server_options)
-
-  Process.sleep(500)
-
-  {:ok, socket} = :gen_tcp.connect(ip_addr, port, [], :infinity)
-
-  :gen_tcp.send(socket, "Hello World")
-  # Received: "Hello World"
-  ```
+  The `socket_handler` function passed inside the options should take the socket returned by `:gen_tcp.accept/1` and return `{:ok, pid}`, where the `pid` describes a process, which will be interacting with the socket. `Membrane.RTMP.Source.TcpServer` will grant that process control over the socket via `:gen_tcp.controlling_process/2`.
   """
+
   use Task
 
   @enforce_keys [:port, :listen_options, :socket_handler]
