943b333205b5a84b822544dcd230afcb130a9ed2
Przemyslaw Roznawski <roznawski.przemyslaw@gmail.com>
2022-09-16 12:48:07 +0200

    elixir 1.12

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Membrane.RTMP.Mixfile do
     [
       app: :membrane_rtmp_plugin,
       version: @version,
-      elixir: "~> 1.14",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:unifex, :bundlex] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
