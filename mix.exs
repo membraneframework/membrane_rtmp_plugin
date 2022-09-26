1445d183c58fa03553124f952f3a24c9d4af1489
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-09-26 13:20:13 +0200

    Bump version to 0.7.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTMP.Mixfile do
   use Mix.Project
 
-  @version "0.6.1"
+  @version "0.7.0"
   @github_url "https://github.com/membraneframework/membrane_rtmp_plugin"
 
   def project do
