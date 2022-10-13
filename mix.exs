affb8b4ff59f48fa75100d8cc7fc16faeaed22ef
Przemyslaw Roznawski <roznawski.przemyslaw@gmail.com>
2022-10-13 08:44:37 +0200

    Bumped h264 plugin to 0.22.0

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Membrane.RTMP.Mixfile do
     [
       {:membrane_core, "~> 0.10"},
       {:unifex, "~> 1.0"},
-      {:membrane_h264_ffmpeg_plugin, "~> 0.21"},
+      {:membrane_h264_ffmpeg_plugin, "~> 0.22"},
       {:membrane_aac_plugin, "~> 0.12"},
       {:membrane_mp4_plugin, "~> 0.16"},
       # testing
