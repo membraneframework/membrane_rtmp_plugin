16595b3ed51f23b6ae3cbc85fd12f43197ee8fd2
Philip Giuliani <philip.giuliani@keepinmind.info>
2022-10-14 20:07:01 +0200

    Use av_packet_from_data in order to have a reference counted buffer

--- c_src/membrane_rtmp_plugin/sink/rtmp_sink.c
+++ c_src/membrane_rtmp_plugin/sink/rtmp_sink.c
@@ -134,6 +134,10 @@ UNIFEX_TERM write_video_frame(UnifexEnv *env, State *state,
       state->output_ctx->streams[state->video_stream_index]->time_base;
   AVPacket *packet = av_packet_alloc();
 
+  uint8_t* data = (uint8_t *)av_malloc(frame->size);
+  memcpy(data, frame->data, frame->size);
+  av_packet_from_data(packet, data, frame->size);
+
   UNIFEX_TERM write_frame_result;
 
   if (is_key_frame) {
@@ -141,16 +145,12 @@ UNIFEX_TERM write_video_frame(UnifexEnv *env, State *state,
   }
 
   packet->stream_index = state->video_stream_index;
-  packet->size = frame->size;
-  packet->data = (uint8_t *)av_malloc(frame->size);
 
   if (!packet->data) {
     write_frame_result =
         unifex_raise(env, "Failed allocating video frame data");
     goto end;
   }
-
-  memcpy(packet->data, frame->data, frame->size);
 
   int64_t dts_scaled =
       av_rescale_q(dts, MEMBRANE_TIME_BASE, video_stream_time_base);
@@ -170,8 +170,7 @@ UNIFEX_TERM write_video_frame(UnifexEnv *env, State *state,
   write_frame_result = write_video_frame_result_ok(env, state);
 
 end:
-  av_free(packet->data);
-  av_packet_free(&packet);
+  av_packet_unref(packet);
   return write_frame_result;
 }
 
@@ -187,18 +186,20 @@ UNIFEX_TERM write_audio_frame(UnifexEnv *env, State *state,
       state->output_ctx->streams[state->audio_stream_index]->time_base;
   AVPacket *packet = av_packet_alloc();
 
+  uint8_t* data = (uint8_t *)av_malloc(frame->size);
+  memcpy(data, frame->data, frame->size);
+  av_packet_from_data(packet, data, frame->size);
+
   UNIFEX_TERM write_frame_result;
 
   packet->stream_index = state->audio_stream_index;
-  packet->size = frame->size;
 
-  packet->data = (uint8_t *)av_malloc(frame->size);
   if (!packet->data) {
     write_frame_result =
         unifex_raise(env, "Failed allocating audio frame data.");
     goto end;
   }
-  memcpy(packet->data, frame->data, frame->size);
+
   int64_t pts_scaled =
       av_rescale_q(pts, MEMBRANE_TIME_BASE, audio_stream_time_base);
   // Packet DTS is set to PTS since AAC buffers do not contain DTS
@@ -216,8 +217,7 @@ UNIFEX_TERM write_audio_frame(UnifexEnv *env, State *state,
   write_frame_result = write_audio_frame_result_ok(env, state);
 
 end:
-  av_free(packet->data);
-  av_packet_free(&packet);
+  av_packet_unref(packet);
   return write_frame_result;
 }
 
