3cb20162c3c556aa712940ebf35773e6b4eb6ae4
Przemysław Rożnawski <roznawski.przemyslaw@gmail.com>
2023-04-05 11:09:23 +0200

    release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTMP.Mixfile do
   use Mix.Project
 
-  @version "0.11.1"
+  @version "0.11.2"
   @github_url "https://github.com/membraneframework/membrane_rtmp_plugin"
 
   def project do
