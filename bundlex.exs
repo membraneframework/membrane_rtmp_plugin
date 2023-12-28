76570b013b71b595d2b5b36f8c06ea762726725a
Michał Śledź <michalsledz34@gmail.com>
2023-12-28 16:07:57 +0100

    Fix precompiled FFmpeg URL

--- bundlex.exs
+++ bundlex.exs
@@ -1,28 +1,6 @@
 defmodule Membrane.RTMP.BundlexProject do
   use Bundlex.Project
 
-  defp get_ffmpeg_url() do
-    membrane_precompiled_url_prefix =
-      "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/latest/download/ffmpeg"
-
-    case Bundlex.get_target() do
-      %{architecture: "aarch64", os: "linux"} ->
-        "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n6.0-latest-linuxarm64-gpl-shared-6.0.tar.xz"
-
-      %{os: "linux"} ->
-        "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n6.0-latest-linux64-gpl-shared-6.0.tar.xz"
-
-      %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
-        "#{membrane_precompiled_url_prefix}_macos_intel.tar.gz"
-
-      %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
-        "#{membrane_precompiled_url_prefix}_macos_arm.tar.gz"
-
-      _other ->
-        nil
-    end
-  end
-
   def project do
     [
       natives: natives()
@@ -38,7 +16,8 @@ defmodule Membrane.RTMP.BundlexProject do
         preprocessor: Unifex,
         os_deps: [
           ffmpeg: [
-            {:precompiled, get_ffmpeg_url(), ["libavformat", "libavutil"]},
+            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg),
+             ["libavformat", "libavutil"]},
             {:pkg_config, ["libavformat", "libavutil"]}
           ]
         ]
