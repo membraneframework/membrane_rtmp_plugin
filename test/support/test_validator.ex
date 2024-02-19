d45094210726b50f949662d04caa4addb5e2b786
Łukasz Kita <lukasz.kita0@gmail.com>
2024-02-19 14:57:49 +0100

    Implement `validate_on_expect_additional_media` for the TestValidator to fix a CI failure

--- test/support/test_validator.ex
+++ test/support/test_validator.ex
@@ -35,4 +35,7 @@ defimpl Membrane.RTMP.MessageValidator, for: Support.TestValidator do
 
   @impl true
   def validate_on_meta_data(_impl, _message), do: {:ok, "on meta data success"}
+
+  @impl true
+  def validate_on_expect_additional_media(_impl, _message), do: {:ok, "on expect additional media success"}
 end
