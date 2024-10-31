88653ccd55c0326ff6d8a063fe1edaf8ed5bf308
Łukasz Kita <lukasz.kita0@gmail.com>
2024-10-31 16:41:29 +0100

    Fix a bug with :stream_deleted message not handled in source bin. Warn instead of failing if unrecogized child notification is delivered to the source bin. Bump to v0.27.2

--- lib/membrane_rtmp_plugin/rtmp/source/source_bin.ex
+++ lib/membrane_rtmp_plugin/rtmp/source/source_bin.ex
@@ -15,6 +15,8 @@ defmodule Membrane.RTMP.SourceBin do
 
   alias Membrane.{AAC, H264, RTMP}
 
+  require Membrane.Logger
+
   def_output_pad :video,
     accepted_format: H264,
     availability: :on_request
@@ -113,6 +115,18 @@ defmodule Membrane.RTMP.SourceBin do
 
   def handle_child_notification(:unexpected_socket_closed, :src, _ctx, state) do
     {[notify_parent: :unexpected_socket_close], state}
+  end
+
+  def handle_child_notification(:stream_deleted, :src, _ctx, state) do
+    {[notify_parent: :stream_deleted], state}
+  end
+
+  def handle_child_notification(notification, child, _ctx, state) do
+    Membrane.Logger.warning(
+      "Received unrecognized child notification from: #{inspect(child)}: #{inspect(notification)}"
+    )
+
+    {[], state}
   end
 
   @doc """
