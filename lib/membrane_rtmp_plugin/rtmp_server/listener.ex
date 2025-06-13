5436b95ea326c088e35ab56f1cbfde213e96e0a2
Oleg Okunevych <okunevichov@gmail.com>
2025-06-13 18:59:26 +0300

    Add RTMPS support

--- lib/membrane_rtmp_plugin/rtmp_server/listener.ex
+++ lib/membrane_rtmp_plugin/rtmp_server/listener.ex
@@ -39,13 +39,59 @@ defmodule Membrane.RTMPServer.Listener do
       end
 
     {:ok, socket} = options.socket_module.listen(options.port, listen_options)
-    send(options.server, {:port, :inet.port(socket)})
+
+    port =
+      case options.socket_module do
+        :gen_tcp ->
+          {:ok, {_ip, port}} = :inet.sockname(socket)
+          port
+
+        :ssl ->
+          {:ok, {_ip, port}} = :ssl.sockname(socket)
+          port
+      end
+
+    send(options.server, {:port, port})
+
+    # send(options.server, {:port, :inet.port(socket)}) |> dbg()
 
     accept_loop(socket, options)
   end
 
   defp accept_loop(socket, options) do
-    {:ok, client} = :gen_tcp.accept(socket)
+    client =
+      case options.socket_module do
+        :gen_tcp ->
+          {:ok, client} = :gen_tcp.accept(socket)
+          client
+
+        :ssl ->
+          {:ok, client} = :ssl.transport_accept(socket)
+          Logger.debug("SSL transport accept successful, starting handshake...")
+
+          # Start with very basic SSL options
+          ssl_opts = [
+            verify: :verify_none,
+            fail_if_no_peer_cert: false
+          ]
+
+          case :ssl.handshake(client, ssl_opts, 10000) do
+            {:ok, ssl_socket} ->
+              Logger.info("SSL handshake successful")
+              ssl_socket
+
+            :ok ->
+              Logger.info("SSL handshake successful (ok)")
+              client
+
+            {:error, reason} ->
+              Logger.error("SSL handshake failed: #{inspect(reason)}")
+              :ssl.close(client)
+              accept_loop(socket, options)
+          end
+      end
+
+    client |> dbg()
 
     {:ok, client_reference} =
       GenServer.start_link(ClientHandler,
@@ -56,7 +102,7 @@ defmodule Membrane.RTMPServer.Listener do
         client_timeout: options.client_timeout
       )
 
-    case :gen_tcp.controlling_process(client, client_reference) do
+    case options.socket_module.controlling_process(client, client_reference) do
       :ok ->
         send(client_reference, :control_granted)
 
